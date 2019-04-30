5464c11be369ddb7c8b3ce360aaff55020c647e7
Gamila <gamilaaah@gmail.com>
2019-04-30 17:02:48 +0200

    Ernst code modified

--- Task (3)/ErnstAngle.py
+++ Task (3)/ErnstAngle.py
@@ -4,23 +4,30 @@ Created on Wed Apr 24 09:11:30 2019
 
 @author: Gamila
 """
-import math
+import numpy as np
 import functionsForTask3
 
 siganl =0
 maximumSignal=0
+flipAngleArray=0
+signalArray=0
+
 
 for flipAngle in range (180): #for SSFP
+    flipAngleArray.append(flipAngle)
     for counter in range (10):
        signal=functionsForTask3.rotationAroundXFunction(phantomSize, flipAngle, magneticVector)
-       signal+=signal
+       signalArray.append(signal)
+       #signal+=signal we need the last magnetization vector not average
+    signalArray.append(signal)
+
        
     if signal> maximumSignal:
          maximumSignal=signal
          print(flipAngle) 
 
-## for GRE
-flipAngle=math.acose(exp(-TR\T1[j][k]))
+## for GRE #choosing tissue from combobox from tab1 to take its T! and T2
+signal=np.sin(flipAngle)*(1-np.exp(-TR/T1))*np.exp(-TE/T2)/(1-np.cos(flipAngle)*np.exp(-TR/T1))            #using T2*=T2
          
          
        
