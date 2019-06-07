06ea929234ad808bb93bbf22de1023db77ef00d6
Mustafa <mostafa0as3d@gmail.com>
2019-06-07 22:12:39 +0200

    Finishing off!

--- Task (3)/Main.py
+++ Task (3)/Main.py
@@ -618,11 +618,11 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     @pyqtSlot()
     def artifact_selection(self):
         current_artifact_string= self.ui.comboBox_4.currentText()
-        if(current_artifact_string == "Artifact #1"):
+        if(current_artifact_string == "Aliasing"):
             Artifacts.NonUnifromSampling_kspace(self)
             self.ARTIFACT_1 = True
             self.ARTIFACT_2 = False
-        elif (current_artifact_string == "Artifact #2"):
+        elif (current_artifact_string == "Non-Uniform Sampling"):
             Artifacts.Aliasing_kspace(self)
             self.ARTIFACT_2 = True
             self.ARTIFACT_1 = False
